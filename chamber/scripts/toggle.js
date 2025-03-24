bbe6bfae084b09631b5d1c52787063698ea025c1
ander508 <off24001@byui.edu>
2025-03-24 13:06:48 +0100

    update

--- chamber/scripts/toggle.js
+++ chamber/scripts/toggle.js
@@ -7,6 +7,79 @@ hamburger.addEventListener('click', () =>{
 })
 
 
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const filePath = await fetch('../chamber/data/members.json');
+        
+        if (!filePath.ok) {
+            throw new Error("Incorrect file path");
+        }
+
+        const listView = document.querySelector('.list-view');
+        const member = document.querySelector('.members');
+
+        const data = await filePath.json();
+        const memberData = data.members;
+
+        listView.addEventListener('click', () => {
+            member.innerHTML = ""; 
+            member.classList.add('open');
+            memberData.forEach((mem) => {
+                member.innerHTML += `
+                    <div class="list">
+                        <h2>${mem.name}</h2>
+                        <p>${mem.address}</p>
+                        <div>${mem.phone}</div>
+                        <div><a href="${mem.url}">${mem.url}</a></div>
+                    </div>
+                `;
+            });
+        });
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+    }
+});
 
 
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const filePath = await fetch('../chamber/data/members.json');
+        
+        if (!filePath.ok) {
+            throw new Error("Incorrect file path");
+        }
+
+        const gridView = document.querySelector('.grid-view');
+        const member = document.querySelector('.members');
+
+        const data = await filePath.json();
+        const memberData = data.members;
+
+        gridView.addEventListener('click', () => {
+            member.innerHTML = ""; 
+            // member.classList.add('open');
+            memberData.forEach((mem) => {
+                member.classList.add('view-grid')
+                member.innerHTML += `
+                <div class="card">
+                    <section class="company-name">
+                        <h2>Company Name: ${mem.name}</h2>
+                        <p>${mem.line}</p>
+                    </section>
+                    <section class="company-img">
+                        <img src="${mem.imageurl}" alt="${mem.name}" loading="lazy" width="100" height="100">
+                        <div class="company-contact">
+                            <div>Email: ${mem.email}</div>
+                            <div>Phone: ${mem.phone}</div>
+                            <div>Url: ${mem.url}</div>
+                        </div>
+                    </section>
+                </div>`;
+            });
+        });
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+    }
+});
 
